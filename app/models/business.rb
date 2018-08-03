629fe8ea272d7c792020c3acbaea29da01fd39bb
jonaspreisler <jonas.preisler@g>
2018-08-03 17:24:20 +0800

    Df

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,6 +1,4 @@
 class Business < ApplicationRecord
-	mount_uploader :representative, RepresentativeUploader
-	mount_uploader :logo, LogoUploader
 	has_many :orders
 	belongs_to :user, optional: :true
 end
