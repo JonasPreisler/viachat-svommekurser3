20b32552c3c8a2d0056d2b69b1e2fc1ec8134d75
jonaspreisler <jonas.preisler@g>
2018-08-24 13:48:57 +0800

    layout on lead edit page

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -1,5 +1,6 @@
 class LeadsController < ApplicationController
   before_action :set_lead, only: [:show, :edit, :update, :destroy]
+  layout 'mobile', only: [:edit]
 
   # GET /leads
   # GET /leads.json
