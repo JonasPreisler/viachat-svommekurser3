ac35f97c9b16b5e684d96a3d1988a9151d668180
jonaspreisler <jonas.preisler@g>
2018-08-25 19:19:02 +0800

    fixed property json problem

--- app/controllers/api/properties_controller.rb
+++ app/controllers/api/properties_controller.rb
@@ -6,33 +6,37 @@ module Api
 
     def index
       @properties = Property.all
-      render json: {
-       "messages": [
-          {
-            "attachment":{
-              "type":"template",
-              "payload":{
-                "template_type":"generic",
-                "image_aspect_ratio": "square",
-                "elements":[
-                  {
-                    "title":"#{@properties.first.address}",
-                    "image_url":"https://thumb7.shutterstock.com/display_pic_with_logo/109564/436440067/stock-vector-human-resources-management-select-employee-recruitment-concept-of-human-resources-management-cv-436440067.jpg",
-                    "subtitle":"#{@properties.first.price}",
-                    "buttons":[
-                      {
-                        "type":"web_url",
-                        "url":"http://chatestate.herokuapp.com/properties/#{@properties.first.id}",
-                        "title":"Click here!"
-                      }
-                    ]
-                  }
-                ]
+      @properties_to_render = [] 
+      @properties.map do |property|
+        @properties_to_render << {
+         "messages": [
+            {
+              "attachment":{
+                "type":"template",
+                "payload":{
+                  "template_type":"generic",
+                  "image_aspect_ratio":"square",
+                  "elements":[
+                    {
+                      "title":"#{property.address}",
+                      "image_url":"https://thumb7.shutterstock.com/display_pic_with_logo/109564/436440067/stock-vector-human-resources-management-select-employee-recruitment-concept-of-human-resources-management-cv-436440067.jpg",
+                      "subtitle":"#{property.price}",
+                      "buttons":[
+                        {
+                          "type":"web_url",
+                          "url":"http://chatestate.herokuapp.com/properties/#{property.id}",
+                          "title":"Click here!"
+                        }
+                      ]
+                    }
+                  ]
+                }
               }
             }
-          }
-        ]
-      }
+          ]
+        }
+      end
+      render json: @properties_to_render
     end
 
     def show
