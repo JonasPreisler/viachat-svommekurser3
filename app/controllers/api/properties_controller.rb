69d1d75ac654838d95bb8b2daa0ed0411c93a18e
jonaspreisler <jonas.preisler@g>
2018-08-25 17:10:58 +0800

    kfdkkdkdddd

--- app/controllers/api/properties_controller.rb
+++ app/controllers/api/properties_controller.rb
@@ -6,32 +6,33 @@ module Api
 
     def index
       @properties = Property.all
-        @properties_to_render = {
-          :messages => [
-            {
-              :type => "template",
-              :payload => {
-                :template_type => "generic",
-                :image_aspect_ratio => "square",
-                :elements => [
+      render json: {
+       "messages": [
+          {
+            "attachment":{
+              "type":"template",
+              "payload":{
+                "template_type":"generic",
+                "image_aspect_ratio": "square",
+                "elements":[
                   {
-                    :title => "#{@properties.second.address}",
-                    :image_url => "https://thumb7.shutterstock.com/display_pic_with_logo/109564/436440067/stock-vector-human-resources-management-select-employee-recruitment-concept-of-human-resources-management-cv-436440067.jpg",
-                    :subtitle => "$ #{@properties.second.price}",
-                    :buttons => [
+                    "title":"#{@properties.second.address}",
+                    "image_url":"https://thumb7.shutterstock.com/display_pic_with_logo/109564/436440067/stock-vector-human-resources-management-select-employee-recruitment-concept-of-human-resources-management-cv-436440067.jpg",
+                    "subtitle":"#{@properties.second.price}",
+                    "buttons":[
                       {
-                        :type => "web_url",
-                        :url => "http://chatestate.herokuapp.com/properties/#{@properties.second.id}",
-                        :title => "Click here."
-                      } 
+                        "type":"web_url",
+                        "url":"http://chatestate.herokuapp.com/properties/#{@properties.second.id}",
+                        "title":"Click here!"
+                      }
                     ]
                   }
                 ]
               }
             }
-          ]
-        }
-      render :json => @properties_to_render
+          }
+        ]
+      }
     end
 
     def show
