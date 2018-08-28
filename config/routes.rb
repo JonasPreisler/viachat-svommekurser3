6fbe86c9d96d85dbdfb049f113791d6d32df6ff2
jonaspreisler <jonas.preisler@g>
2018-08-28 18:52:03 +0800

    new design

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'pages#landing'
   scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
     resources :property_images
     resources :properties
