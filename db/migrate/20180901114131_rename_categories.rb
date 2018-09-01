6f1d73f4f2d1bcd66df0cf5da209992eaf608f34
jonaspreisler <jonas.preisler@g>
2018-09-01 19:57:13 +0800

    Ke

--- db/migrate/20180901114131_rename_categories.rb
+++
@@ -1,5 +0,0 @@
-class RenameCategories < ActiveRecord::Migration[5.2]
-  def change
-  	rename_column :leads, :category, :property_type
-  end
-end
--- db/migrate/20180901115332_add_category_to_leads.rb
+++
@@ -1,5 +0,0 @@
-class AddCategoryToLeads < ActiveRecord::Migration[5.2]
-  def change
-    add_column :leads, :category, :string
-  end
-end
