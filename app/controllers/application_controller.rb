62bc21eac17708c50267b299ef5f3e5ac2ccf810
jonaspreisler <jonas.preisler@g>
2018-09-20 01:02:18 +0800

    removed something locale in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,13 +7,5 @@ class ApplicationController < ActionController::Base
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:secret])
     end
-    
-	def set_locale
-		I18n.locale = params[:locale] if params[:locale].present?
-	end
-
-	def default_url_options(options = {})
-		{locale: I18n.locale}
-	end
 
 end
