12510ad74966ca57d7c9e3b601cb7336c16f2960
jonaspreisler <jonas.preisler@g>
2018-09-29 02:03:20 +0800

    Fixed Program number in api controller

--- app/controllers/api/program19/users_controller.rb
+++ app/controllers/api/program19/users_controller.rb
@@ -1,4 +1,4 @@
-class Api::Program9::UsersController < ApplicationController
+class Api::Program19::UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
   skip_before_action :verify_authenticity_token
 
