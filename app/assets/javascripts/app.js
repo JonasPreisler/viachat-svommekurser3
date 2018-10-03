e49385e67f6834180bcf90eeac198ab3f1938859
jonaspreisler <jonas.preisler@g>
2018-10-04 01:56:46 +0800

    Fds

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,4 +1,4 @@
-particlesJS.load('particles-js', 'assets/particles.json', function() {
+$().ready(function () {
+	particlesJS.load('particles-js', 'assets/particles.json', function() {
   console.log('callback - particles.js config loaded');
-});
-
+})});
